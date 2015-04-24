ed39655b6503cf0067d70f997f9c29e927ad0398
HnLn <hans.langouche@gmail.com>
2015-04-24 10:48:36 +0200

    MSS-132 better checking

--- CultureFeed/CultureFeed/Uitpas/Passholder/EventActions.php
+++ CultureFeed/CultureFeed/Uitpas/Passholder/EventActions.php
@@ -44,7 +44,7 @@ class CultureFeed_Uitpas_Passholder_EventActions {
     $eventActions->pointsPromotions = CultureFeed_Uitpas_Passholder_PointsPromotionResultSet::createFromXML($xml->xpath('pointsPromotions', FALSE), 'pointsPromotion');
 
     $eventCheckin = $xml->xpath('eventCheckin', FALSE);
-    if (!empty($eventCheckin)) {
+    if ($eventCheckin instanceof CultureFeed_SimpleXMLElement) {
       $eventActions->eventCheckin = CultureFeed_Uitpas_Passholder_EventCheckin::createFromXML($eventCheckin);
     }
     $eventBuyTicket = $xml->xpath('eventBuyTicket', FALSE);

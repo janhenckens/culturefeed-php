1ddeb4343546eac3c217c0d46ab1802ff8774f12
Lieven Goudeseune <lieven.goudeseune@wunderkraut.com>
2015-12-03 16:23:54 +0100

    CUL-13: Updated unit test for toPostData.

--- CultureFeed/test/ActivityTest.php
+++ CultureFeed/test/ActivityTest.php
@@ -2,13 +2,15 @@
 
 class CultureFeed_ActivityTest extends PHPUnit_Framework_TestCase {
 
-  public function testToPostData() {
+  public function testPriorityToPostData() {
 
     $activity = new CultureFeed_Activity();
-    $activity->priority = CultureFeed_Activity::PRIORITY_NORMAL;
-    
-    $result = $activity->toPostData();
 
+    $result = $activity->toPostData();
+    $this->assertFALSE(array_key_exists('priority', $result));
+    
+    $activity->priority = CultureFeed_Activity::PRIORITY_NORMAL;
+    $result = $activity->toPostData();
     $this->assertTRUE(array_key_exists('priority', $result));
   }
 }
